bdd3267bfbcb5261a1ab1b49dcd733227307341f
konart <konapt@gmail.com>
2017-02-22 18:46:24 +0300

    Moved websockets logic to the Table component

--- src/Card.jsx
+++ src/Card.jsx
@@ -10,49 +10,8 @@ class Card extends Component {
             background: {}
         };
         this.toggleBackground = this.toggleBackground.bind(this);
-        this.appendLog = this.appendLog.bind(this)
     }
 
-    appendLog(item) {
-        console.log(item)
-        // var doScroll = log.scrollTop === log.scrollHeight - log.clientHeight;
-        // log.appendChild(item);
-        // if (doScroll) {
-        //     log.scrollTop = log.scrollHeight - log.clientHeight;
-        // }
-    }
-
-    componentWillMount() {
-        let conn;
-        let msg = document.getElementById("msg");
-        let log = document.getElementById("log");
-
-        if (window["WebSocket"]) {
-            conn = new WebSocket("ws://localhost:8080/ws");
-                conn.onopen = function (m) {
-                    conn.send("test");
-                    console.log("test");
-                };
-            conn.onclose = function (evt) {
-                let item = document.createElement("div");
-                item.innerHTML = "<b>Connection closed.</b>";
-                this.appendLog(item);
-            };
-            conn.onmessage = function (evt) {
-                let messages = evt.data.split('\n');
-                for (let i = 0; i < messages.length; i++) {
-                    let item = document.createElement("div");
-                    item.innerText = messages[i];
-                    console.log(item);
-                    //this.appendLog(item);
-                }
-            };
-        } else {
-            let item = document.createElement("div");
-            item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
-            this.appendLog(item);
-        }
-    }
     toggleBackground() {
         //ReactDOM.render(<Welcome />, document.getElementById("deck"));
         if (this.state.image == "url(image.png)") {
--- src/Table.jsx
+++ src/Table.jsx
@@ -2,6 +2,36 @@ import React, {Component} from 'react';
 
 class Table extends Component {
 
+    componentWillMount() {
+        let conn;
+        let msg = document.getElementById("msg");
+        let log = document.getElementById("log");
+
+        if (window["WebSocket"]) {
+            conn = new WebSocket("ws://localhost:8080/ws");
+            conn.onopen = function (m) {
+                conn.send("test");
+                console.log("test");
+            };
+            conn.onclose = function (evt) {
+                let item = document.createElement("div");
+                item.innerHTML = "<b>Connection closed.</b>";
+                this.appendLog(item);
+            };
+            conn.onmessage = function (evt) {
+                let messages = evt.data.split('\n');
+                for (let i = 0; i < messages.length; i++) {
+                    let item = document.createElement("div");
+                    item.innerText = messages[i];
+                    console.log(item);
+                }
+            };
+        } else {
+            let item = document.createElement("div");
+            item.innerHTML = "<b>Your browser does not support WebSockets.</b>";
+        }
+    }
+
     render() {
         return (
             <div id="table">
